e3e222a199ad5055c2001b7b8cf9d3e11c103a06
Nafi-GOAT <nafih2989@gmail.com>
2025-11-07 21:37:09 -0800

    1

---
+++ simulation.cpp
@@ -0,0 +1,13 @@
+//
+//
+// COMSC 210 || Mehraj Hasan Nafi
+// IDE Used : Visual Studio Code
+//
+//
+#include <iostream>
+#include <string>
+#include <map>
+#include <array>
+#include <list>
+#include <iomanip>
+using namespace std;
