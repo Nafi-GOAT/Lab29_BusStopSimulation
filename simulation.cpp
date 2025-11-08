b853b62de58dd0d9ee8c0202115187b26c001acf
Nafi-GOAT <nafih2989@gmail.com>
2025-11-07 21:45:33 -0800

    3

--- simulation.cpp
+++ simulation.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 // ---- stages ----
 enum Stage { WAITING = 0, ONBUS = 1, LEFT = 2 };
-using Buckets = array<list<string>, 2>;
+using Buckets = array<list<string>, 3>;
 using Env = map<string, Buckets>;
+
 
